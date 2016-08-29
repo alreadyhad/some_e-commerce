7d531947dafc593c7d39b4fddfba7748f523776e
alreadyhad <alreadyhad@hotmail.com>
2016-08-29 10:44:27 +0800

    demo

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+
+window.onload = function(){
+	
+/**
+ * [text description]
+ * @param  {[type]} a [description]
+ * @param  {[type]} b [description]
+ * @param  {[type]} c [description]
+ * @return {[type]}   [description]
+ * @async
+ */
+	function text(a,b,c){ 
+
+
+	}
+	
+		
+}
